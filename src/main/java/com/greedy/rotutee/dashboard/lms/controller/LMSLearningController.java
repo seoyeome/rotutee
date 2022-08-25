c6be1c7d433ab521432cbccf6e8dcb7d16fdd455
seoyeome <seoyeomedev@gmail.com>
2022-08-25 20:30:52 +0900

    [FIX] 주석 수정

--- src/main/java/com/greedy/rotutee/dashboard/lms/controller/LMSLearningController.java
+++ src/main/java/com/greedy/rotutee/dashboard/lms/controller/LMSLearningController.java
@@ -82,7 +82,6 @@ public class LMSLearningController {
         int timeNo = Integer.parseInt(request.getParameter("timeNo"));
         String status = request.getParameter("status");
         System.out.println("timeNo = " + timeNo);
-        System.out.println("웨에에에에에에에에에에에에에에에엥 = " + status);
 
         lmsLearningService.modifyVideoWatchingStatus(timeNo, status);
 
